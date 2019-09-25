add608a6f509877ecfef371238e44183aea37534
henrikfjell <h1rik.f@gmail.com>
2019-09-25 15:56:48 +0200

    task 4 completed

---
+++ øving3-4.jl
@@ -0,0 +1,41 @@
+# Task 4, splitting. Notice line 12 - that some real shit!
+
+function splitintwo(x,y)
+    x_left = x[1:Int(ceil(length(x)/4)), :]
+    x_right = x[Int(ceil(length(x)/4)) + 1:end, : ]
+    y_left = zeros(typeof(y[1]), (0, 2))
+    y_right = zeros(typeof(y[1]), (0, 2))
+    
+    
+    # Iterate through y
+    for i = 1:size(y, 1)
+        # element = [y[i,1] y[i,2]] This and the next line is  the same
+        element = reshape(y[i, :], (1, 2))
+        if inarray(element, x_left)
+            y_left = [y_left; element]
+        else
+            y_right = [y_right; element]
+        end
+    end
+    
+    return x_left, x_right, y_left, y_right
+end
+
+
+function inarray(sub, x)
+    for i = 1:size(x, 1)
+        xi = x[i, :]
+        if sub[1] == xi[1] && sub[2] == xi[2]
+            return true
+        end
+    end
+    return false
+end
+
+
+x = [1.0 2.0; 2.0 3.0; 3.0 2.0; 4.0 5.0; 6.0 6.0; 7.0 1.0]
+y = [7.0 1.0; 1.0 2.0; 3.0 2.0; 2.0 3.0; 4.0 5.0; 6.0 6.0]
+
+a, b, c, d = splitintwo(x,y)
+
+println(a, b, c, d)
