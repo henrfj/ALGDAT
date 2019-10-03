bb444c1d6f27d6a0a5cd9664c4b9f4c97388ebb0
Henrik Fjellheim <h1rik.f@gmail.com>
2019-10-03 09:58:39 +0200

    got well under way with task 1, practical

---
+++ 4 oving/øving 4-1.jl
@@ -0,0 +1,38 @@
+"""Implementering av counting sort på strenger"""
+
+A = ["hei", "nei", "skje", "felt", "hodetelefon", "sylofon"]
+
+function countingsortletters(A,position)
+    # Antar at alle element i A er lange nok til å inneholde 'position'
+    # Anta at alle bokstaver i strengene er mellom a og z
+    C = []
+
+    for i in 1:26
+        push!(C, 0)
+    end
+
+    for j in 1:length(A)
+        C[chartodigit(A[j][position])] += 1
+    end
+
+    for i in 2:26
+        C[i] += C[i - 1]
+    end
+    # C is now cumelative, with C[i] containing number of elements less or equal to i
+    for j in length(A):1
+        
+    end
+
+end
+
+
+
+function chartodigit(character)
+    #Dette er en hjelpefunksjon for å få omgjort en char til tall
+    #Den konverterer 'a' til 1, 'b' til 2 osv.
+    #Eksempel: chartodigit("hei"[2]) gir 5 siden 'e' er den femte bokstaven i alfabetet.
+    #` er bokstaven rett før a i ASCII, slik at a blir 1
+    return character - '`'
+end
+
+
--- testing.jl
+++ testing.jl
@@ -14,4 +14,31 @@ y = A[4:end, :]
 # --------------------------------------------------------
 yleft  = zeros(typeof(y[1]), (0, 2))
 
-println(y_left, yleft)
+# println(yleft)
+
+# --------------------------------------------------------
+function chartodigit(character)
+    #Dette er en hjelpefunksjon for å få omgjort en char til tall
+    #Den konverterer 'a' til 1, 'b' til 2 osv.
+    #Eksempel: chartodigit("hei"[2]) gir 5 siden 'e' er den femte bokstaven i alfabetet.
+    #` er bokstaven rett før a i ASCII, slik at a blir 1
+    return character - '`'
+end
+
+C = []
+for i in 1:26
+    push!(C, 0)
+end
+
+# C = zeros(typeof(1), (26, 1))
+# dett blir en matrise
+
+# println(C)
+A = ["hai", "nzi", "skje", "felt", "hodetelefon", "sylofon"]
+#print(A[5][5])
+
+for j in 1:length(A)
+    C[chartodigit(A[j][2])] += 1
+end
+
+print(C)
