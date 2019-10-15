3c7a48796a01e6023942c6564345375192e57122
henrikfjell <h1rik.f@gmail.com>
2019-10-15 11:57:03 +0200

    question 2 godkjent

---
+++ 6 oving/cumulative.jl
@@ -0,0 +1,46 @@
+
+
+function printmatrix(M)
+    """
+    Author of this printmatrix is Eirik-ff,
+    give him a follow on github, he deserves some luv 💕
+    """
+    rows, cols = size(M)
+    for i = 1:rows
+        for j = 1:cols
+            print(M[i, j], " ")
+        end
+        println()
+    end 
+end
+
+
+function cumulative(weights)
+
+
+
+    
+end
+
+
+
+
+
+
+### Tester ###
+# Disse testene blir kjør når du kjører filen
+# Du trenger ikke å endre noe her, men du kan eksperimentere!
+
+printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+
+using Test
+@testset "Tester" begin
+	@test cumulative([1 1 ; 1 1]) == [1 1 ;2 2]
+    #Dette er samme eksempel som det vist i oppgaveteskten
+	@test cumulative([3  6  8 6 3; 7  6  5 7 3; 4  10 4 1 6; 10 4  3 1 2;6  1  7 3 9])== [3  6  8  6  3;10 9  11 10 6;13 19 13 7  12;23 17 10 8  9;23 11 15 11 17]
+end
+
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n")
---
+++ 6 oving/lis.jl
@@ -0,0 +1,43 @@
+"""
+Question 2: Finne den lengste økende delfølgen
+
+Altså ikke bare lengden, men selve delfølgen
+Svaret er ikke alltid unikt, men da skal vi returnere den delfølgen som kommer først i sekvensen.
+
+"""
+
+function lis(s, mls)
+    # Vi får inn mls! Altså får vi inn scoren til den beste delfølgen mls,
+    # bergenet iterativt bottum-up, sammen med selve følgen .
+    ml = maximum(mls)
+    lis = zeros(Int, ml)
+    for i = length(mls):-1:1
+        # Bakover traversering,
+        # fordi beste løsningen ligger bakerst
+        # #Top-Down
+        if ml == mls[i]
+            # Vil alltid trigger første loop
+            lis[ml] = s[i]
+            ml -= 1
+        end
+    end
+    return lis
+end
+
+### Tester ###
+# Disse testene blir kjør når du kjører filen
+# Du trenger ikke å endre noe her, men du kan eksperimentere!
+
+printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+
+using Test
+@testset "Tester" begin
+	@test lis([5,3,3,6,7],[1, 1, 1, 2, 3]) == [3,6,7]
+	@test lis([2,2,2,2],[1, 1, 1, 1]) == [2]
+	@test lis([100,50,25,10],[1, 1, 1, 1]) == [10]
+	@test lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15],[1, 2, 2, 3, 2, 3, 3, 4, 2, 4, 3, 5, 3, 5, 4, 6]) == [0,2,6,9,11,15]
+end
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n")
